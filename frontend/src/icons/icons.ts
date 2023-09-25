2781fefb4f9b24c42a8d9201e45c80171380e0af
Olena Babii <gracetomlinson26@gmail.com>
2023-09-25 23:47:33 +0300

    created header

---
+++ frontend/src/icons/icons.ts
@@ -0,0 +1,7 @@
+import logo from "./logo.svg";
+
+const icons = {
+  logo: logo,
+};
+
+export { icons };
---
+++ frontend/src/ui/common/Header.tsx
@@ -0,0 +1,13 @@
+import { icons } from "../../icons/icons";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={icons.logo} alt="logo" />
+      <h1>Superheroes</h1>
+    </header>
+  );
+};
+
+export default Header;
