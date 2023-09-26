6229012b02d31c9f6a666c807b6fb754952fd219
Olena Babii <gracetomlinson26@gmail.com>
2023-09-26 15:24:36 +0300

    add superhero creating page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CreateSuperheroPage from "./pages/CreateSuperheroPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new-superhero" element={<CreateSuperheroPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/pages/CreateSuperheroPage.tsx
+++ frontend/src/pages/CreateSuperheroPage.tsx
@@ -1,9 +1,13 @@
 import Header from "../ui/common/header/Header";
+import Form from "../ui/forms/Form";
 
 const CreateSuperheroPage = () => {
   return (
     <>
       <Header />
+      <main>
+        <Form title="Create new superhero"/>
+      </main>
     </>
   );
 };
