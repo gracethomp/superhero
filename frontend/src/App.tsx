90aa74dc6f264fae771997c0f69d941380bead5d
Olena Babii <gracetomlinson26@gmail.com>
2023-09-26 15:30:35 +0300

    routes configuration: home and create pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateSuperheroPage from "./pages/CreateSuperheroPage";
+import { routes } from "./utils/routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/new-superhero" element={<CreateSuperheroPage />} />
+        <Route path={routes.home} element={<HomePage />} />
+        <Route path={routes.newHero} element={<CreateSuperheroPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/ui/common/header/Header.tsx
+++ frontend/src/ui/common/header/Header.tsx
@@ -1,17 +1,23 @@
+import { NavLink } from "react-router-dom";
 import { icons } from "../../../icons/icons";
 import Button from "../button/Button";
 import "./Header.css";
+import { routes } from "../../../utils/routes";
 
 const Header = () => {
   return (
     <header>
-      <div className="header-logo">
-        <img src={icons.logo} alt="logo" />
-        <p className="logo-text">Superheroes</p>
-      </div>
-      <Button variant="secondary" icon={icons.plus}>
-        Add new superhero
-      </Button>
+      <NavLink to={routes.home}>
+        <div className="header-logo">
+          <img src={icons.logo} alt="logo" />
+          <p className="logo-text">Superheroes</p>
+        </div>
+      </NavLink>
+      <NavLink to={routes.newHero}>
+        <Button variant="secondary" icon={icons.plus}>
+          Add new superhero
+        </Button>
+      </NavLink>
     </header>
   );
 };
